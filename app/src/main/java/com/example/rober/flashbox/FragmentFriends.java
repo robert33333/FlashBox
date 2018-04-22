ca66c183b245ff93f38d7585a31c12ab0f328190
robert33333 <robert.banu18@gmail.com>
2018-04-22 18:00:24 +0300

    Now works but we need custom adaptor!

--- app/src/main/java/com/example/rober/flashbox/FragmentFriends.java
+++ app/src/main/java/com/example/rober/flashbox/FragmentFriends.java
@@ -6,12 +6,22 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class FragmentFriends extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_friends, container, false);
+        View view = inflater.inflate(R.layout.layout_friends, container, false);
+
+        ListView listView = view.findViewById(R.id.listViewFriends);
+
+        String [] friends = {"unu","doi","trei"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,friends);
+        listView.setAdapter(adapter);
+
+        return view;
     }
 }
