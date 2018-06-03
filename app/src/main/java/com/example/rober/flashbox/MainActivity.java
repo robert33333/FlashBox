0a6d0565dbab545b01cff9dd25a828ecb7b06d4d
maresioan <maresioan19@gmail.com>
2018-06-03 15:43:21 +0300

    Amazing

--- app/src/main/java/com/example/rober/flashbox/MainActivity.java
+++ app/src/main/java/com/example/rober/flashbox/MainActivity.java
@@ -110,7 +110,6 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
     */
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
